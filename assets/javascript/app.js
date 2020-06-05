af8253d02242fdbed6a2139fe4c5f47bc661a15a
Mike Vong <mv910@hotmail.com>
2020-06-05 00:18:08 -0700

    change Home section

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,29 +1,35 @@
 $(document).ready(function(){
-    // Add smooth scrolling to all links
-    $("a").on('click', function(event) {
-  
-      // Make sure this.hash has a value before overriding default behavior
+
+  $("a").on('click', function(event) {
       if (this.hash !== "") {
-        // Prevent default anchor click behavior
         event.preventDefault();
-  
-        // Store hash
         var hash = this.hash;
-  
-        // Using jQuery's animate() method to add smooth page scroll
-        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+
         $('html, body').animate({
           scrollTop: $(hash).offset().top
         }, 800, function(){
-     
-          // Add hash (#) to URL when done scrolling (default click behavior)
           window.location.hash = hash;
         });
-      } // End if
+      };
+    var delay = 500;
+    $(".progress-bar").each(function(i) {
+    $(this).delay(delay * i).animate({
+    width: $(this).attr('aria-valuenow') + '%'
+    }, delay);
 
+    $(this).prop('Counter', 0).animate({
+      Counter: $(this).text()}, {
+      duration: delay,
+      easing: 'swing',
+      step: function(now) {
+        $(this).text(Math.ceil(now) + '%');
+        }
+      });
+    $(".progress-bar").addClass("notransition");
+    $('.progress-bar').attr('style', "width: 0%");
     });
-
-
+  });
+  
 
 
 
